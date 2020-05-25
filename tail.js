705d583f46c6235ef6218cb2971c80c3bf81cfaf
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-05-25 23:30:50 +0000

    Created tail method.

---
+++ tail.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+	if (actual !== expected){
+		console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+	} else {
+		console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+	}
+};
+
+const tail = function(arr){
+	newArr = arr.slice(1);
+	return newArr;
+}
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
