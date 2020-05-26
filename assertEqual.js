5effbd47b801eb59cb16eb10f794dd67e38a3aff
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-05-26 00:12:38 +0000

    Got eslint working.

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
-	if (actual !== expected){
-		console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-	} else {
-		console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-	}
+  if (actual !== expected) {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
--- head.js
+++ head.js
@@ -1,14 +1,14 @@
 const assertEqual = function(actual, expected) {
-	if (actual !== expected){
-		console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-	} else {
-		console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-	}
+  if (actual !== expected) {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
 };
 
-const head = function(arr){
-	return arr[0];
-}
+const head = function(arr) {
+  return arr[0];
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
