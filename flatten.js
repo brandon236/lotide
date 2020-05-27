1b435765d724eedac7f9e24308b4f47f17e919c9
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-05-27 06:46:28 +0000

    Flattens any given 2d array into a single list.

---
+++ flatten.js
@@ -0,0 +1,17 @@
+const flatten = function(arr) {
+  let flat = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        flat.push(arr[i][j]);
+      }
+    } else {
+      flat.push(arr[i]);
+    }
+  }
+  return flat;
+};
+
+console.log(flatten([1,2,[3,4],5,[6]]));
+console.log(flatten([[1],2,3,[4, 3, 4, 1],5]));
+console.log(flatten([["Lighthouse", "Labs"]]));
