4e045f2640a8e893550a6467da073bed55d709c3
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-05-27 02:05:37 +0000

    Check to see if arrays are equal and then prints out a message.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -14,7 +14,7 @@ const eqArrays = function(arr1, arr2) {
   }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-    console.log(`❌❌❌Assertion Failed: ${arr1} !== ${arr2}`);
+    console.log("❌❌❌Assertion Failed: " + arr1 + " !== " + arr2);
     return false;
     }
   }
