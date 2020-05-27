710e165b01273b724623a10249d9c2f3b6aac112
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-05-27 23:36:32 +0000

    Finds the middle of a given array.

--- middle.js
+++ middle.js
@@ -1,3 +1,32 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    console.log(`❌❌❌Assertion Failed: ${arr1} !== ${arr2}`);
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+    console.log("❌❌❌Assertion Failed: " + arr1 + " !== " + arr2);
+    return false;
+    }
+  }
+    console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
+    return true;
+};
+
+
 const middle = function (arr) {
   if (arr.length % 2 === 0 && arr.length > 2) {
     const middle = arr.length / 2;
